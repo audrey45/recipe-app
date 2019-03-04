1f88344b55337189f61fe6bc5ff134502f456203
=audrey45 <arceniokas@gmail.com>
2019-03-04 13:08:46 +0800

    show list of recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+public recipes: Recipe[] =[
+  new Recipe("Test Name","Test Description", "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1823,w_2731,x_0,y_485/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/SFS_Pinto_Bean_and_Swiss_Chard_Enchiladas_021_4__ioqi23.jpg"),
+  new Recipe("Test Name","Test Description", "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1823,w_2731,x_0,y_485/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/SFS_Pinto_Bean_and_Swiss_Chard_Enchiladas_021_4__ioqi23.jpg"),
+];
+
 
   constructor() { }
 
