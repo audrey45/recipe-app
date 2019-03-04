5e0c10b65817a6e2c77632fb69c2e79ae5b163e7
=audrey45 <arceniokas@gmail.com>
2019-03-04 18:54:51 +0800

    -create RecipeService and ShoppingListService. -Get Recipe in Service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from 'src/app/shared/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,13 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  public recipes: Recipe[] = [
-    new Recipe("Test Name", "Test Description", "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1823,w_2731,x_0,y_485/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/SFS_Pinto_Bean_and_Swiss_Chard_Enchiladas_021_4__ioqi23.jpg"),
-    new Recipe("Test Name 2", "Test Description 2", "https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA"),
-  ];
+  public recipes: Recipe[];
 
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {
+    this.recipes = this.recipeService.getRecipes();
+   }
 
   ngOnInit() {
   }
---
+++ src/app/shared/recipe.service.ts
@@ -0,0 +1,14 @@
+import { Recipe } from '../recipes/recipe.model';
+
+export class RecipeService{
+    private recipes: Recipe[] = [
+        new Recipe("Test Name", "Test Description", "https://res.cloudinary.com/sagacity/image/upload/c_crop,h_1823,w_2731,x_0,y_485/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/SFS_Pinto_Bean_and_Swiss_Chard_Enchiladas_021_4__ioqi23.jpg"),
+        new Recipe("Test Name 2", "Test Description 2", "https://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/1506120378/MR_0917170472.jpg?itok=aWyDp3CA"),
+      ];
+
+      getRecipes(){
+          return this.recipes.slice();
+      }
+    
+
+}
