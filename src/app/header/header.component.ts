3dceb5b9df6593ff4699b66795d52a2a963fc4e3
=audrey45 <arceniokas@gmail.com>
2019-03-04 15:21:31 +0800

    add simple Navigation by header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent {
+    @Output() featuredSelected = new EventEmitter<string>();
 
+    select(feature: string) {
+        this.featuredSelected.emit(feature);
+    }
 
 }
